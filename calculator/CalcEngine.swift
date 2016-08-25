b3b79b14be352a47143e0fd5d46015d666176943
Yevgen Berberyan <egnbjork@gmail.com>
2016-08-25 19:38:37 +0300

    updated up to lesson 2

--- calculator/CalcEngine.swift
+++ calculator/CalcEngine.swift
@@ -9,13 +9,65 @@
 import Foundation
 
 class CalcEngine{
-    func setOperand(operand: Double){}
     
-    func performOperation(symbol: String){}
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double){
+        accumulator = operand
+    }
+    
+    var operations: Dictionary<String, Operation> = [
+        "π": Operation.Constant(M_PI),
+        "e": Operation.Constant(M_E),
+        "√": Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "×": Operation.BinaryOperation({$0*$1}),
+        "÷": Operation.BinaryOperation({$0/$1}),
+        "+": Operation.BinaryOperation({$0+$1}),
+        "−": Operation.BinaryOperation({$0-$1}),
+        "=": Operation.Equals
+    ]
+    
+    enum Operation{
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double,Double) -> Double)
+        case Equals
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    func performOperation(symbol: String){
+        if let operation = operations[symbol]{
+            switch operation{
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunc: function, firstOperand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+            }
+        }
+    }
+    
+    private func executePendingBinaryOperation(){
+        if pending != nil {
+            accumulator = pending!.binaryFunc(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    struct PendingBinaryOperationInfo{
+        var binaryFunc:(Double,Double)->Double
+        var firstOperand: Double
+    }
     
     var result: Double{
         get{
-            return 0.0
+            return accumulator
         }
     }
 }
